4795487b82c6d5c1364547345e83b14b0ce064fa
jim price <shadeops@gmail.com>
2018-05-04 01:09:53 -0400

    Basic trianglemesh support

--- soho/python2.7/PBRTgeo.py
+++ soho/python2.7/PBRTgeo.py
@@ -38,7 +38,6 @@ def sphere_wrangler(gdp, paramset=None):
             xform = gdp.value(prim_xform_h, prim_num)
             api.ConcatTransform(xform)
             api.Shape('sphere', paramset)
-    return
 
 def disk_wrangler(gdp, paramset=None):
     num_prims = gdp.globalValue('geo:primcount')[0]
@@ -48,33 +47,47 @@ def disk_wrangler(gdp, paramset=None):
             xform = gdp.value(prim_xform_h, prim_num)
             api.ConcatTransform(xform)
             api.Shape('disk', paramset)
-    return
 
 def tube_wrangler(gdp, paramset=None):
     if paramset is None:
         paramset = ParamSet()
     num_prims = gdp.globalValue('geo:primcount')[0]
     prim_xform_h = gdp.attribute('geo:prim', 'geo:primtransform')
+    # SOHO BUG
     # This always returns 1, so use 'intrinsic:tubetaper' instead
     # taper_h = gdp.attribute('geo:prim', 'geo:tubetaper')
     taper_h = gdp.attribute('geo:prim', 'intrinsic:tubetaper')
     closed_h = gdp.attribute('geo:prim', 'geo:primclose')
+    prim_h = gdp.attribute('geo:prim', 'geo:number')
 
     for prim_num in xrange(num_prims):
         shape_paramset = copy.copy(paramset)
         with api.TransformBlock():
             xform = gdp.value(prim_xform_h, prim_num)
             taper = gdp.value(taper_h, prim_num)[0]
+
+            # workaround, see TODO below
+            if not (taper == 0 or taper == 1):
+                geo_num = gdp.value(prim_h, prim_num)[0]
+                api.Comment('Skipping tube, prim # %i, with non-conforming taper of %f' %
+                                (geo_num,taper))
+                continue
+
             closed = gdp.value(closed_h, prim_num)[0]
             api.ConcatTransform(xform)
             api.Rotate(-90, 1, 0, 0)
             if taper == 0:
                 shape = 'cone'
                 api.Translate(0, 0, -0.5)
-            else:
+            elif taper == 1:
                 shape = 'cylinder'
                 shape_paramset.add(PBRTParam('float', 'zmin', -0.5))
                 shape_paramset.add(PBRTParam('float', 'zmax', 0.5))
+            else:
+                # TODO support hyperboloid, however pbrt currently
+                # has no ends of trouble with this shape type
+                # crashes or hangs
+                pass
             api.Shape(shape, shape_paramset)
 
             if closed:
@@ -87,12 +100,51 @@ def tube_wrangler(gdp, paramset=None):
                 else:
                     disk_paramset.add(PBRTParam('float', 'height', 0))
                     api.Shape('disk', disk_paramset)
-    return
 
+def vtx_attrib_gen(gdp, attrib):
+    num_prims = gdp.globalValue('geo:primcount')[0]
+    vtx_count_h = gdp.attribute('geo:prim', 'geo:vertexcount')
+    for prim in xrange(num_prims):
+        num_vtx = gdp.value(vtx_count_h, prim)[0]
+        for vtx in xrange(num_vtx):
+        #for vtx in xrange(num_vtx-1,-1,-1):
+            # could also use gdp.vertex(attrib, prim, vtx)
+            value = gdp.value(attrib, (prim,vtx))
+            yield value
+
+def geo_attrib_gen(gdp, attrib, count):
+    for i in xrange(count):
+        value = gdp.value(attrib, i)
+        yield value
+
+def trianglemesh_wrangler(gdp, paramset=None):
+    if paramset is None:
+        mesh_paramset = ParamSet()
+    else:
+        mesh_paramset = copy.copy(paramset)
+
+    # We can only deal with triangles, where Houdini is a bit more
+    # general, so we'll need to tesselate
+    options = {'geo:triangulate' : True}
+    options = {'geo:polysides' : 3}
+
+    mesh_gdp = gdp.tesselate(options)
+
+    num_pts = gdp.globalValue('geo:pointcount')[0]
+
+    p_attrib = mesh_gdp.attribute('geo:point', 'P')
+    p_ref_attrib = mesh_gdp.attribute('geo:vertex', 'geo:pointref')
+
+    P = geo_attrib_gen(mesh_gdp, p_attrib, num_pts)
+    indices = vtx_attrib_gen(mesh_gdp, p_ref_attrib)
+    mesh_paramset.add(PBRTParam('integer', 'indices', indices))
+    mesh_paramset.add(PBRTParam('point','P', P))
+    api.Shape('trianglemesh', mesh_paramset)
 
 shape_wranglers = { 'Sphere': sphere_wrangler,
                     'Circle' : disk_wrangler,
                     'Tube' : tube_wrangler,
+                    'Poly' : trianglemesh_wrangler,
                   }
 
 def shape_splits(gdp):
@@ -104,7 +156,6 @@ def shape_splits(gdp):
     for prim_num in xrange(num_prims):
         prim_name = gdp.value(prim_name_h, prim_num)[0]
         yield prim_name
-    return
 
 def save_geo(soppath, now):
     # split by material
